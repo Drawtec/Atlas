c3556b6adeef3dd53d5c043f780f617367a8fa57
Drawtec <darkxell133@gmail.com>
2025-01-11 23:37:34 +0100

    Update AtlasSettings.cs
    added settings for ControllerMode and hide failed maps
    Added group for uniques maps

--- Atlas/AtlasSettings.cs
+++ Atlas/AtlasSettings.cs
@@ -9,9 +9,11 @@ namespace Atlas
         public Vector4 DefaultBackgroundColor = new(0.0f, 0.0f, 0.0f, 0.4f);
         public Vector4 DefaultFontColor = new(1.0f, 1.0f, 1.0f, 1.0f);
         public bool HideCompletedMaps = true;
+        public bool HideFailedMaps = true;
         public float ScaleMultiplier = 1.0f;
         public List<MapGroupSettings> MapGroups = [];
         public string GroupNameInput = string.Empty;
+        public bool ControllerMode = false;
 
         public AtlasSettings()
         {
@@ -19,6 +21,11 @@ namespace Atlas
             citadels.Maps.Add("The Copper Citadel");
             citadels.Maps.Add("The Iron Citadel");
             citadels.Maps.Add("The Stone Citadel");
+
+            var unique = new MapGroupSettings("Unique", new Vector4(1.0f, 0.5f, 0.0f, 0.6f), new Vector4(0.0f, 0.0f, 0.0f, 1.0f));
+            citadels.Maps.Add("Untainted Paradise");
+            citadels.Maps.Add("Vaults of Kamasa");
+            citadels.Maps.Add("Moment of Zen");
 
             var good = new MapGroupSettings("Good", new Vector4(0.0f, 1.0f, 0.0f, 0.4f), new Vector4(1.0f, 1.0f, 1.0f, 1.0f));
             good.Maps.Add("Burial Bog");
@@ -33,6 +40,7 @@ namespace Atlas
 
             MapGroups.Add(citadels);
             MapGroups.Add(good);
+            MapGroups.Add(unique);
         }
     }
 
